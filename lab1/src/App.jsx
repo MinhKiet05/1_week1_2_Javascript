6299d025871111f6caa3496f2699d855b3f5aaa8
MinhKiet05 <tranvanbetu1965@gmail.com>
2026-01-18 22:36:40 +0700

    Xong React Bai 2 Button + bổ sung hình ảnh cho bài 1

--- lab1/src/App.jsx
+++ lab1/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import ProductCard from './components/productCard/ProductCard.jsx';
+import Button from './components/Button/Button.jsx';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <ProductCard image="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQcrJXU7zI6Dv1c-iq3gHMYsqbr8qZ9Y1nDJUlP3BHPbgyH9Au3vr2ukUf90bBgfHtq_VHM-ttXhWubn89KnuJJMBKp_qEByEjLK1_dcm_XB3VgqrxuGHblVvkY" name="Sample Product" price={100000} />
+      <div style={{ display: "flex", gap: "16px" }}>
+      <Button type="primary">Primary</Button>
+      <Button type="danger">Danger</Button>
+      <Button type="success">Success</Button>
+      </div>
     </>
   )
 }
---
+++ lab1/src/components/button/Button.jsx
@@ -0,0 +1,8 @@
+import './Button.css';
+export default function Button({ type = "primary", children }) {
+  return (
+    <button className={`btn btn-${type}`}>
+      {children}
+    </button>
+  );
+}
