029d7f652f51e1df7f60a6cc06d9190a0de7f96a
MinhKiet05 <tranvanbetu1965@gmail.com>
2026-01-19 15:18:43 +0700

    Xong react tuan 1 bai 5

--- lab1/src/App.jsx
+++ lab1/src/App.jsx
@@ -1,21 +1,10 @@
-import { useState } from 'react'
+
 import './App.css'
-import LoginForm from './components/loginForm/LoginForm.jsx'
+import ProductList from './components/productList/ProductList.jsx'
 function App() {
-  const [open, setOpen] = useState(false);
   return (
     <>
-      <div style={{ padding: "40px" }}>
-      <button onClick={() => setOpen(true)}>
-        Open Login
-      </button>
-
-      <LoginForm
-        isOpen={open}
-        type="primary"
-        onClose={() => setOpen(false)}
-      />
-    </div>
+      <ProductList />
     </>
   )
 }
